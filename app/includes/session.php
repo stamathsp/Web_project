87dc4b90a26431ee373df0221b5cdf53e08acd0b
stamathsp <stamatispetrou63@gmail.com>
2025-05-19 23:30:23 +0300

    :D

--- app/includes/session.php
+++ app/includes/session.php
@@ -1,7 +1,21 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])) {
+
+$protected_pages = [
+    '/profiles/view.php',
+    '/profiles/edit.php',
+    '/profiles/delete.php',
+    '/playlists/create.php',
+    '/playlists/edit.php',
+    '/videos/add.php',
+    '/videos/play.php',
+    '/search/search.php',
+];
+
+$current_page = $_SERVER['PHP_SELF'];
+
+if (in_array($current_page, $protected_pages) && !isset($_SESSION['user_id'])) {
     $_SESSION['redirect_to'] = $_SERVER['REQUEST_URI'];
-    header("Location: /index.php");
+    header("Location: /auth/login.php");
     exit;
 }
