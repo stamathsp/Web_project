dd6f8f7cdd956e331f5876c1da90a8fc458a8cf9
stamathsp <stamatispetrou63@gmail.com>
2025-05-20 12:42:24 +0300

    changed some files

--- app/includes/db.php
+++ app/includes/db.php
@@ -1,18 +1,19 @@
 <?php
-$host = 'localhost';
-$dbname = 'streamzone';
-$user = 'root';
-$pass = '';
+$host = 'db'; //database host
+$db   = 'web_project';
+$user = 'user';
+$pass = 'password';
 $charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
 $options = [
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $pass, $options);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
-    die("Σφάλμα σύνδεσης: " . $e->getMessage());
+    exit("Σφάλμα σύνδεσης: " . $e->getMessage());
 }
